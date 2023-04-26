2cc72b52e8e315362789415af5b794864a62f8d6
Daniel.C <cheadaniel.95@gmail.com>
2023-04-26 15:01:09 +0200

    navbar modifier et ajout de bouton

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -40,6 +40,7 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+            $user->setRoles(["ROLE_USER"]);
 
             $entityManager->persist($user);
             $entityManager->flush();
